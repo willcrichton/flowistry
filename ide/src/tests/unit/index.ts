6049c8a87d7ca85846d7b7b31c159dfe0856df84
Connor Fogarty <connorflynnfogarty@gmail.com>
2021-10-25 15:03:02 -0500

    Reduce test timeout to 10 seconds

--- ide/src/tests/unit/index.ts
+++ ide/src/tests/unit/index.ts
@@ -19,7 +19,7 @@ export async function run(): Promise<void> {
 
         try {
             // Run the mocha test
-            mocha.timeout(100000);
+            mocha.timeout(10000);
             mocha.run(failures => {
                 if (failures > 0) {
                     reject(new Error(`${failures} tests failed.`));
