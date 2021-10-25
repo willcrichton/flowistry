c1249bc3c4d1a7eca82424af92a14cf431916a12
Connor Fogarty <connorflynnfogarty@gmail.com>
2021-10-25 15:02:39 -0500

    Rewrite `glob` files `Mocha` mapping

--- ide/src/tests/unit/index.ts
+++ ide/src/tests/unit/index.ts
@@ -15,7 +15,7 @@ export async function run(): Promise<void> {
 
     return new Promise((resolve, reject) => {
         // Add files to the test suite
-        files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
+        files.map(f => path.resolve(testsRoot, f)).forEach(mocha.addFile);
 
         try {
             // Run the mocha test
