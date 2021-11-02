8fcf9d8a188585fcb5d9cc4e88671cf9d2b6f28f
Connor Fogarty <connorflynnfogarty@gmail.com>
2021-11-02 15:13:04 -0500

    Add `slice` helper function for tests

--- ide/src/tests/unit/extension.test.ts
+++ ide/src/tests/unit/extension.test.ts
@@ -4,26 +4,35 @@ import { suite, before, describe, it } from "mocha";
 import _ from "lodash";
 import vscode from "vscode";
 import forward_slice from "../mock_data/forward_slice.json";
+import { MOCK_PROJECT_FILES } from "../util/constants";
+
+const slice = async (
+  direction: 'forward' | 'backward',
+  position: [[number, number], [number, number]],
+  filename: string,
+): Promise<void> => {
+  const file = vscode.Uri.parse(filename);
+  await vscode.window.showTextDocument(file);
+
+  const start_position = new vscode.Position(position[0][0], position[0][1]);
+  const end_position = new vscode.Position(position[1][0], position[1][1]);
+
+  vscode.window.activeTextEditor!.selection = new vscode.Selection(
+    start_position,
+    end_position
+  );
+
+  await vscode.commands.executeCommand(`flowistry.${direction}_select`);
+}
 
 suite("Extension Test Suite", () => {
   before(async () => {
     chai.use(deepEqualAnyOrder);
-
-    vscode.window.showInformationMessage("Start all tests");
-    await vscode.window.showTextDocument(vscode.workspace.textDocuments[0]);
-
-    // Select 'x' on line 3 of forward_slice.rs
-    const startXPos = new vscode.Position(2, 12);
-    const endXPos = new vscode.Position(2, 13);
-    vscode.window.activeTextEditor!.selection = new vscode.Selection(
-      startXPos,
-      endXPos
-    );
   });
 
   describe("Forward slice", () => {
     it("of constant highlights correct values", async () => {
-      await vscode.commands.executeCommand("flowistry.forward_select");
+      await slice("forward", [[2, 12], [2, 13]], MOCK_PROJECT_FILES.forward_slice);
 
       // Ugly workaround to get the values from the Selection class
       const actualSelection = JSON.parse(
