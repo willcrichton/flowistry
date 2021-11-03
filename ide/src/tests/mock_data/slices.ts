3c30142f4569199eae3c490497c7b1eca96312b9
Connor Fogarty <connorflynnfogarty@gmail.com>
2021-11-03 10:15:09 -0500

    Add all forward slice tests

--- ide/src/tests/mock_data/slices.ts
+++ ide/src/tests/mock_data/slices.ts
@@ -20,4 +20,40 @@ export default [
     "direction": "forward",
     "slice_on": [[2, 12], [2, 13]]
   },
+  {
+    "test": "basic_unused",
+    "file": MOCK_PROJECT_FILES.forward_slice,
+    "direction": "forward",
+    "slice_on": [[7, 4], [7, 14]]
+  },
+  {
+    "test": "basic_update",
+    "file": MOCK_PROJECT_FILES.forward_slice,
+    "direction": "forward",
+    "slice_on": [[13, 4], [13, 18]]
+  },
+  {
+    "test": "condition",
+    "file": MOCK_PROJECT_FILES.forward_slice,
+    "direction": "forward",
+    "slice_on": [[19, 4], [19, 14]]
+  },
+  {
+    "test": "pointer_write",
+    "file": MOCK_PROJECT_FILES.forward_slice,
+    "direction": "forward",
+    "slice_on": [[30, 4], [30, 18]]
+  },
+  {
+    "test": "function_params",
+    "file": MOCK_PROJECT_FILES.forward_slice,
+    "direction": "forward",
+    "slice_on": [[35, 19], [35, 20]]
+  },
+  {
+    "test": "struct_param",
+    "file": MOCK_PROJECT_FILES.forward_slice,
+    "direction": "forward",
+    "slice_on": [[41, 16], [41, 17]]
+  },
 ] as TestSlice[];
--- ide/src/tests/mock_project/src/forward_slice.rs
+++ ide/src/tests/mock_project/src/forward_slice.rs
@@ -1,5 +1,44 @@
-pub fn main() {
+pub fn basic_constant_and_variable() {
     let mut x = 1;
     let y = x + 2;
     let z = y;
+}
+
+pub fn basic_unused() {
+    let x = 1;
+    let y = 1 + 2;
+    let z = x + y;
+}
+
+pub fn basic_update() {
+    let mut x = 1;
+    x += 1;
+    let y = x;
+}
+
+fn condition() {
+    let x = 1;
+    let y = 2;
+    let z = if true {
+        x
+    } else {
+        y
+    };
+    let w = z;
+}
+
+fn pointer_write() {
+    let mut x = 1;
+    let y = &mut x;
+    *y += 2;
+}
+
+fn function_params(x: i32) {
+    let y = x + 1;
+    let z = y;
+}
+
+struct Point(i32, i32);
+fn struct_param(p: &mut Point) {
+  p.0 += 1;
 }
