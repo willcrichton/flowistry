f584e23e60ea25eb4bb84bfe4181ffc9980eb626
Connor Fogarty <connorflynnfogarty@gmail.com>
2021-11-18 21:40:31 -0600

    Simplify slice selection tests

--- ide/src/tests/slice/mock_data/slices.ts
+++ ide/src/tests/slice/mock_data/slices.ts
@@ -7,26 +7,20 @@ export type TestSlice = {
   slice_on: [[number, number], [number, number]];
 };
 
-const forward_slices = [
+export const forward_slices: TestSlice[] = [
   {
     "test": "constant",
     "slice_on": [[1, 16], [1, 17]],
+    "file": MOCK_PROJECT_FILES.forward_slice,
+    "direction": "forward",
   },
-].map((slice) => ({
-  "file": MOCK_PROJECT_FILES.forward_slice,
-  "direction": "forward",
-  ...slice,
-}));
+];
 
-const backward_slices = [
+export const backward_slices: TestSlice[] = [
   {
     "test": "variable_read",
     "slice_on": [[2, 4], [2, 5]],
+    "file": MOCK_PROJECT_FILES.backward_slice,
+    "direction": "backward",
   },
-].map((slice) => ({
-  "file": MOCK_PROJECT_FILES.backward_slice,
-  "direction": "backward",
-  ...slice,
-}));
-
-export default [...forward_slices, ...backward_slices] as TestSlice[];
+];
--- ide/src/tests/slice/select.test.ts
+++ ide/src/tests/slice/select.test.ts
@@ -1,27 +1,28 @@
 import chai, { expect } from "chai";
 import deepEqualAnyOrder from 'deep-equal-in-any-order';
-import { suite, before, describe, it } from "mocha";
+import { suite, before, test } from "mocha";
 import _ from "lodash";
-import slices from "./mock_data/slices";
+import { forward_slices, backward_slices } from "./mock_data/slices";
 import { get_slice_selections, resolve_sequentially } from "./util/slice_helpers";
 
 suite("Slice Selection Tests", async () => {
   before(async function () {
     chai.use(deepEqualAnyOrder);
+  });
 
-    // Run slices synchronously to avoid overlapping selections 
-    const slice_test_cases = await resolve_sequentially(slices, get_slice_selections);
+  test("Forward select", async () => {
+    const forward_selections = await resolve_sequentially(forward_slices, get_slice_selections);
 
-    ["forward", "backward"].forEach((direction) => {
-      describe(`${direction} select`, function () {
-        _.filter(slice_test_cases, ['direction', direction]).forEach((test_case) => {
-          it(test_case.test, () => {
-            expect(test_case.expected_selections).to.be.deep.equalInAnyOrder(test_case.actual_selections);
-          });
-        });
-      });
+    forward_selections.forEach(async (selection) => {
+      expect(selection.expected_selections).to.be.deep.equalInAnyOrder(selection.actual_selections);
     });
   });
 
-  it('This is a required placeholder to allow before() to work');
+  test("Backward select", async () => {
+    const backward_selections = await resolve_sequentially(backward_slices, get_slice_selections);
+
+    backward_selections.forEach(async (selection) => {
+      expect(selection.expected_selections).to.be.deep.equalInAnyOrder(selection.actual_selections);
+    });
+  });
 });
