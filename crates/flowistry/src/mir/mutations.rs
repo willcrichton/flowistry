48348bef8cbe55247317f7a617434a7f35b2a3a1
Connor Fogarty <connorflynnfogarty@gmail.com>
2021-12-08 10:47:39 -0600

    Use place inputs when finding mutations

--- crates/flowistry/src/mir/mutations.rs
+++ crates/flowistry/src/mir/mutations.rs
@@ -1,18 +1,43 @@
+use std::collections::HashMap;
+
 use rustc_hir::Mutability;
 use rustc_middle::{
   mir::{
     visit::{PlaceContext, Visitor},
-    Body, HasLocalDecls, Location, Place,
+    Body, HasLocalDecls, Location, Place, Rvalue,
   },
   ty::TyCtxt,
 };
 
 use super::aliases::Aliases;
+use crate::mir::utils::PlaceCollector;
+
+struct GatherInputs<'tcx> {
+  inputs: HashMap<Place<'tcx>, Vec<Place<'tcx>>>,
+}
+
+impl Visitor<'tcx> for GatherInputs<'tcx> {
+  fn visit_assign(
+    &mut self,
+    place: &Place<'tcx>,
+    rvalue: &Rvalue<'tcx>,
+    location: Location,
+  ) {
+    let mut collector = PlaceCollector::default();
+    collector.visit_rvalue(rvalue, location);
+    
+    if collector.places.is_empty() {
+      collector.places.push(*place);
+    }
+
+    self.inputs.insert(*place, collector.places);
+  }
+}
 
 struct FindMutations<'a, 'tcx> {
   tcx: TyCtxt<'tcx>,
   body: &'a Body<'tcx>,
-  place: Place<'tcx>,
+  places: Vec<Place<'tcx>>,
   aliases: Aliases<'tcx>,
   locations: Vec<Location>,
 }
@@ -29,7 +54,7 @@ impl Visitor<'tcx> for FindMutations<'a, 'tcx> {
       place.iter_projections().all(|(sub_place, _)| {
         let ty = sub_place.ty(self.body.local_decls(), self.tcx).ty;
         !matches!(ty.ref_mutability(), Some(Mutability::Not))
-      }) && *place == self.place
+      }) && self.places.contains(place)
     });
 
     if _context.is_mutating_use() && place_has_conflicts {
@@ -44,10 +69,21 @@ pub fn find_mutations(
   place: Place<'tcx>,
   aliases: Aliases<'tcx>,
 ) -> Vec<Location> {
+  let mut input_gatherer = GatherInputs {
+    inputs: HashMap::new(),
+  };
+  input_gatherer.visit_body(body);
+
+  let places = input_gatherer
+    .inputs
+    .get(&place)
+    .cloned()
+    .unwrap_or_default();
+
   let mut finder = FindMutations {
     tcx,
     body,
-    place,
+    places,
     aliases,
     locations: Vec::new(),
   };
