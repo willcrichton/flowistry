e8cb57817b9a30a52d1430431eda08ab513c6734
Connor Fogarty <connorflynnfogarty@gmail.com>
2022-01-08 17:19:38 -0600

    Use `ModularMutationVisitor` to find mutations

--- crates/flowistry/src/mir/mutations.rs
+++ crates/flowistry/src/mir/mutations.rs
@@ -1,43 +1,15 @@
-use rustc_hir::{def_id::DefId, Mutability};
+use log::debug;
+use rustc_hir::def_id::DefId;
 use rustc_middle::{
-  mir::{
-    visit::{PlaceContext, Visitor},
-    Body, HasLocalDecls, Location, Place
-  },
+  mir::{visit::Visitor, Body, Location, Place},
   ty::TyCtxt,
 };
 
 use super::aliases::Aliases;
-use crate::{infoflow::mutation::ModularMutationVisitor};
-
-struct FindMutations<'a, 'tcx> {
-  tcx: TyCtxt<'tcx>,
-  body: &'a Body<'tcx>,
-  places: Vec<Place<'tcx>>,
-  aliases: Aliases<'tcx>,
-  locations: Vec<Location>,
-}
-
-impl Visitor<'tcx> for FindMutations<'a, 'tcx> {
-  fn visit_place(
-    &mut self,
-    place: &Place<'tcx>,
-    _context: PlaceContext,
-    _location: Location,
-  ) {
-    let all_conflicts = self.aliases.conflicts(place);
-    let place_has_conflicts = all_conflicts.iter().any(|place| {
-      place.iter_projections().all(|(sub_place, _)| {
-        let ty = sub_place.ty(self.body.local_decls(), self.tcx).ty;
-        !matches!(ty.ref_mutability(), Some(Mutability::Not))
-      }) && self.places.contains(place)
-    });
-
-    if _context.is_mutating_use() && place_has_conflicts {
-      self.locations.push(_location);
-    }
-  }
-}
+use crate::{
+  indexed::IndexSetIteratorExt, infoflow::mutation::ModularMutationVisitor,
+  mir::utils::PlaceExt,
+};
 
 pub fn find_mutations(
   tcx: TyCtxt<'tcx>,
@@ -46,27 +18,36 @@ pub fn find_mutations(
   place: Place<'tcx>,
   aliases: Aliases<'tcx>,
 ) -> Vec<Location> {
-  let mut places = vec![];
+  let mut locations = vec![];
+  let pointer_aliases = place
+    .interior_pointers(tcx, body, def_id)
+    .into_values()
+    .map(|v| v.into_iter().map(|(place, _)| place))
+    .flatten()
+    .map(|place| aliases.aliases.row(tcx.mk_place_deref(place)).copied())
+    .flatten()
+    .chain(vec![place])
+    .collect_indices(aliases.place_domain.clone());
 
-  ModularMutationVisitor::new(tcx, body, def_id, |visitor_place, inputs, _, _| {
-    if place == visitor_place {
-      places = if inputs.is_empty() {
-        vec![place]
-      } else {
-        inputs
-      }
-    }
-  })
-  .visit_body(body);
+  debug!("pointer aliases: {:?}", pointer_aliases);
 
-  let mut finder = FindMutations {
+  ModularMutationVisitor::new(
     tcx,
     body,
-    places,
-    aliases,
-    locations: Vec::new(),
-  };
-  finder.visit_body(body);
+    def_id,
+    |mutated_place, _, mutated_location, _| {
+      debug!("checking mutated location {:?}", mutated_location);
 
-  finder.locations
+      let mut place_conflicts = aliases.conflicts(mutated_place);
+      place_conflicts.intersect(&pointer_aliases);
+
+      if place_conflicts.len() > 0 {
+        debug!("  found conflicts: {:?}", place_conflicts);
+        locations.push(mutated_location);
+      }
+    },
+  )
+  .visit_body(body);
+
+  locations
 }
--- crates/flowistry_ide/tests/mutations.rs
+++ crates/flowistry_ide/tests/mutations.rs
@@ -31,9 +31,23 @@ fn tuple_field_write_find_only_field() {
   let src = r#"
 fn main() {
   `[let mut x = `[(0, 0)]`;]`
-  `[`[x.0 += 1]`;]`
-  x.1 += 1;
-  `(x.0)`;
+  x.0 += 1;
+  `[`[x.1 += 1]`;]`
+  `[`[`(&x.1)`]`;]`
+}
+"#;
+
+  utils::find_mutations(src);
+}
+
+#[test]
+fn tuple_field_mutable_ref_mutation() {
+  let src = r#"
+fn main() {
+  `[let `(mut y)` = `[0]`;]`
+  let mut x = (0, &mut y);
+  x.0 += 1;
+  `[`[*x.1 += 1]`;]`
 }
 "#;
 
@@ -45,7 +59,7 @@ fn update_mutable_borrow() {
   let src = r#"
 fn main() {
   `[let `(mut x)` = `[1]`;]`
-  `[let y = `[&mut x]`;]`
+  let y = &mut x;
   `[`[*y += 1]`;]`
 }
 "#;
@@ -59,7 +73,7 @@ fn mutable_function_arg() {
 fn update(x: &mut i32) {}
 fn main() {
   `[let `(mut x)` = `[1]`;]`
-  `[update(`[&mut x]`);]`
+  `[`[update(&mut x)]`;]`
 }
 "#;
 
@@ -71,9 +85,8 @@ fn struct_write_find_whole_struct() {
   let src = r#"
 fn main() {
   struct Foo { x: i32, y: i32 }
-  `[let mut x = `[Foo { x: 1, y: 2 }]`;]`
+  `[let `(mut x)` = `[Foo { x: 1, y: 2 }]`;]`
   `[`[x.y = 3]`;]`
-  `(x)`;
 }
 "#;
 
@@ -88,7 +101,7 @@ fn main() {
   `[let mut x = `[Foo { x: 1, y: 2 }]`;]`
   `[`[x.y = 3]`;]`
   x.x = 3;
-  `(x.y)`;
+  `[`[`(&x.y)`]`;]`
 }
 "#;
 
@@ -99,11 +112,98 @@ fn main() {
 fn struct_method_mutable_self() {
   let src = r#"
 struct Foo();
-impl Foo { fn bar(&mut self) {} }
+impl Foo { fn bar(&mut self) {} fn baz(&self) {} }
 fn main() {
-  `[let mut x = `[Foo {}]`;]`
+  `[let `(mut x)` = `[Foo {}]`;]`
   `[`[x.bar()]`;]`
-  `(x)`;
+  x.baz();
+}
+"#;
+
+  utils::find_mutations(src);
+}
+
+#[test]
+fn struct_mut_ptr_field() {
+  let src = r#"
+fn main() {
+  struct Foo<'a>(&'a mut i32);
+  `[let `(mut x)` = `[1]`;]`
+  let f = Foo(&mut x);
+  `[`[*f.0 += 1]`;]`
+}
+"#;
+
+  utils::find_mutations(src);
+}
+
+#[test]
+fn struct_mut_ptr_function() {
+  let src = r#"
+struct Foo<'a>(&'a mut i32);
+fn foo(f: Foo) {}
+
+fn main() {
+  `[let `(mut x)` = `[0]`;]`
+  let f = Foo(&mut x);
+  `[`[foo(f)]`;]`
+}
+"#;
+
+  utils::find_mutations(src);
+}
+
+#[test]
+fn struct_mut_ptr_method() {
+  let src = r#"
+struct Foo<'a>(&'a mut i32);
+fn foo(f: Foo) {}
+
+fn main() {
+  `[let `(mut x)` = `[0]`;]`
+  let f = Foo(&mut x);
+  `[`[foo(f)]`;]`
+}
+"#;
+
+  utils::find_mutations(src);
+}
+
+#[test]
+fn mut_child_closure() {
+  let src = r#"
+fn main() {
+  `[let `(mut x)` = `[0]`;]`
+  let mut mutate_x = || {
+    x += 1;
+  };
+  `[`[mutate_x()]`;]`
+}
+"#;
+
+  utils::find_mutations(src);
+}
+
+#[test]
+fn array_mut_ptr_mutate_index() {
+  let src = r#"
+fn main() {
+  `[let `(mut x)` = `[0]`;]`
+  let mut y = [&mut x];
+  `[`[*y[0] += 1]`;]`
+}
+"#;
+
+  utils::find_mutations(src);
+}
+
+#[test]
+fn vec_mut_ptr_mutate_index() {
+  let src = r#"
+fn main() {
+  `[let `(mut x)` = `[0]`;]`
+  `[let mut y = `[vec![&mut x]]`;]`
+  `[`[*y[0] += 1]`;]`
 }
 "#;
 
