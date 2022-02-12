f514099579a5e5a5ac30e7f997f479e92e3e2290
Will Crichton <wcrichto@cs.stanford.edu>
2022-02-11 19:32:35 -0800

    Group spanned places in focus mode

--- crates/flowistry_ide/src/focus/mod.rs
+++ crates/flowistry_ide/src/focus/mod.rs
@@ -5,6 +5,7 @@ use flowistry::{
   range::Range,
   source_map,
 };
+use itertools::Itertools;
 use rustc_hir::{BodyId, Expr, ExprKind, Node};
 use rustc_macros::Encodable;
 use rustc_middle::ty::TyCtxt;
@@ -34,50 +35,67 @@ pub fn focus(tcx: TyCtxt<'tcx>, body_id: BodyId) -> Result<FocusOutput> {
   let source_map = tcx.sess.source_map();
   let spanner = source_map::Spanner::new(tcx, body_id, body);
 
-  let targets = spanner
-    .mir_spans
-    .iter()
-    .map(|mir_span| (mir_span.place, mir_span.location))
-    .collect::<Vec<_>>();
-  let relevant = infoflow::compute_dependency_spans(
-    results,
-    targets.clone(),
-    Direction::Both,
-    &spanner,
+  use std::collections::HashMap;
+
+  use rustc_span::SpanData;
+  let mut groups: HashMap<SpanData, usize> = HashMap::default();
+  for mir_span in &spanner.mir_spans {
+    *groups.entry(mir_span.span).or_insert(0) += 1;
+  }
+  log::info!("groups: {:?}", groups.values().collect::<Vec<_>>());
+  log::info!(
+    "avg: {:?} / {:?}",
+    groups.values().filter(|x| **x > 1).sum::<usize>(),
+    spanner.mir_spans.len()
   );
 
-  let slices = spanner
+  let grouped_spans = spanner
     .mir_spans
     .iter()
+    .map(|mir_span| (mir_span.span, (mir_span.place, mir_span.location)))
+    .into_group_map()
+    .into_iter()
+    .collect::<Vec<_>>();
+  let targets = grouped_spans
+    .iter()
+    .map(|(_, target)| target.clone())
+    .collect();
+
+  let relevant =
+    infoflow::compute_dependency_spans(results, targets, Direction::Both, &spanner);
+
+  let slices = grouped_spans
+    .iter()
     .zip(relevant)
-    .filter_map(|(mir_span, relevant)| {
+    .filter_map(|((mir_span, _targets), relevant)| {
       log::debug!("Slice for {mir_span:?} is {relevant:#?}");
-      let range = Range::from_span(mir_span.span.span(), source_map).ok()?;
+      let range = Range::from_span(mir_span.span(), source_map).ok()?;
 
       let slice = relevant
         .into_iter()
         .filter_map(|span| Range::from_span(span, source_map).ok())
         .collect::<Vec<_>>();
 
-      let mutations = find_mutations::find_mutations(
-        tcx,
-        body,
-        def_id.to_def_id(),
-        mir_span.place,
-        &results.analysis.aliases,
-      );
-      let mutations = mutations
-        .into_iter()
-        .flat_map(|location| {
-          spanner.location_to_spans(location, source_map::EnclosingHirSpans::OuterOnly)
-        })
-        .filter_map(|span| Range::from_span(span, source_map).ok())
-        .collect::<Vec<_>>();
+      // TODO: restore this code once find_mutations is integrated
+      // let mutations = find_mutations::find_mutations(
+      //   tcx,
+      //   body,
+      //   def_id.to_def_id(),
+      //   mir_span.place,
+      //   &results.analysis.aliases,
+      // );
+      // let mutations = mutations
+      //   .into_iter()
+      //   .flat_map(|location| {
+      //     spanner.location_to_spans(location, source_map::EnclosingHirSpans::OuterOnly)
+      //   })
+      //   .filter_map(|span| Range::from_span(span, source_map).ok())
+      //   .collect::<Vec<_>>();
 
       Some(PlaceInfo {
         range,
         slice,
-        mutations,
+        mutations: Vec::new(),
       })
     })
     .collect::<Vec<_>>();
