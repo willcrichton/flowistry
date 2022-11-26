52e968ba7060ba88cf099898e91aac9614d1d9e5
Will Crichton <wcrichto@cs.stanford.edu>
2022-11-26 09:14:50 -0600

    Bump to v0.5.33

--- scripts/check_versions.py
+++ scripts/check_versions.py
@@ -3,7 +3,7 @@ import toml
 import json
 
 js_pkg_version = json.load(open('./ide/package.json', 'r'))['version']
-cli_version = sp.check_output('cargo flowistry -V', shell=True).decode('utf-8').split(' ')[1].strip()
+cli_version = sp.check_output('cargo flowistry -V', shell=True).decode('utf-8').strip()
 
 print('JS package version: ', js_pkg_version)
 print('CLI version: ', cli_version)
